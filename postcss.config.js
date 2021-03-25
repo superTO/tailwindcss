ef270f239daa206a956e35f8cb4ddd5223a2af25
superTO <to840116@gmail.com>
2021-03-25 23:49:43 +0800

    Feat: postcss.config.js import '@fullhuman/postcss-purgecss'
    npm run build-css ==> generage new compiled.css(9.89KB)
    **3.74MB -> 9.89KB**

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,14 @@
 // postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ]
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+
+    purgecss({
+      content: ['./**/*.html']
+    })
+  ]
 }
