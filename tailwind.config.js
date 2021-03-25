47d397c03e86659ba6e58df702826cfa1b753fc9
superTO <to840116@gmail.com>
2021-03-25 23:32:28 +0800

    tailwind.config.js purgeCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,30 @@
+// module.exports = {
+//   purge: [
+//     './**/*.html',
+//     './**/*.js',
+//   ],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
 module.exports = {
-  purge: [
-    './**/*.html',
-    './**/*.js',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+  purge: {
+    content: ['./**/*.html'],
+
+    // These options are passed through directly to PurgeCSS
+    options: {
+      safelist: ['bg-red-500', 'px-4'],
+      blocklist: [/^debug-/],
+      keyframes: true,
+      fontFace: true,
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  // ...
 }
